ceec1e4bbca427353ecd2a028f4c213d95424f93
ZFB DD <yeo@163.com>
2022-05-31 12:58:04 +0800

    fix object is not extensible by Object.freeze #1

--- web-index.js
+++ web-index.js
@@ -1,4 +1,4 @@
 import * as spannerGeneric from './libs/generic/index'
 import * as spannerWebRestrict from './libs/web-restrict/index'
 
-window.spanner = Object.assign(spannerGeneric,spannerWebRestrict)
+window.spanner = Object.assign({},spannerGeneric,spannerWebRestrict)
