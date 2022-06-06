7623f9212f360fc8c99b6aabe26802327886eefb
ZFB DD <yeo@163.com>
2022-06-06 09:46:21 +0800

    add concatenate,retryPromise,getParentByClassName #1

--- libs/web-restrict/getParentByClassName.js
+++ libs/web-restrict/getParentByClassName.js
@@ -4,7 +4,7 @@
  * @param {String} className className for parent element to find
  * @returns {Element} dom node to find
  */
-function getParentByClassName(node, className) {
+export default function getParentByClassName(node, className) {
     let parentNode = node, currentNode = node
     while (!(parentNode && parentNode.classList && parentNode.classList.contains(className))) {
         if (currentNode.parentNode) {
